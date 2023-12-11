ac6f81e7284fe7c390ee7be742de93a244a04b5d
halit <halit4068@gmail.com>
2023-12-11 13:15:18 +0100

    v 2.5

---
+++ src/P04.java
@@ -0,0 +1,7 @@
+public class P04 {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba yeni versiyon");
+        System.out.println("Hosgeldin yeni versiyon");
+    }
+}
